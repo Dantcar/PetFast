2f1cf0999555c5a264856d84c7f2b4b0a758ea52
deciodecarvalho <deciodecarvalho@05S006475-187>
2016-09-15 14:03:36 -0300

    Dia 1 Projeto PetFast Conexao com banco DerbyDB - Provisório Crud Cliente

--- petFast/src/visao/EntradaSistemaPetFast.java
+++ petFast/src/visao/EntradaSistemaPetFast.java
@@ -201,11 +201,11 @@ public class EntradaSistemaPetFast extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 894, Short.MAX_VALUE)
+            .addGap(0, 950, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 602, Short.MAX_VALUE)
+            .addGap(0, 671, Short.MAX_VALUE)
         );
 
         pack();
